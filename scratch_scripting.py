29357c2974b3b6240cbc47e6f46f62601060d350
cylitaguy <cylita.guy@gmail.com>
2020-01-18 15:21:37 -0500

    Creating a scratch py file for getting web scraping or api code ready

---
+++ scratch_scripting.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 14:51:33 2020
+
+@author: Cylita
+"""
+
+def hello():
+  
+    print("Hello Spencer")
+    
+hello()
+
+name = "Derek"
+print (name)
+
+
+import random
+import os
+import sys
+
+list_test = ['juice', 'potatoes']
+
+list_test.append = ['pickle']
+del list_test[0]
+print(list_test)
+
+age = 15
+
+if age 
