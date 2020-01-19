f53b866b4c0617e7b2ea98991d0e571de79d05cb
cylitaguy <cylita.guy@gmail.com>
2020-01-19 16:28:06 -0500

    Creating script to scape and build web database

---
+++ scripts/webscraping_articles.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 16:20:31 2020
+
+@author: Cylita
+"""
+
+'''
+Script developed to pull a set of newspaper articles from CBC.ca.
+Script also scrapes related links included in said artles.
+
+'''
+
+import newspaper
+
+
+
