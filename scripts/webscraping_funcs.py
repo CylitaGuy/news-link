e92ad470fd9bbb112f90ccfde4e0c86b928f5613
cylitaguy <cylita.guy@gmail.com>
2020-01-20 10:51:10 -0500

    Creating functions file to scrape all coverapge headlines from a news url landing page

---
+++ scripts/webscraping_funcs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 10:37:53 2020
+
+@author: Cylita
+"""
+
+'''
+Functions file for web scraping articles
+
+'''
+
+#Function for pulling all story urls from a website landing page
+def ScrapeCover(url):
+    
