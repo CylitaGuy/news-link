0980257c7b8b11ec2da5d6fb50b52db7e4544138
cylitaguy <cylita.guy@gmail.com>
2020-01-20 23:27:19 -0500

    Additions to driver script to pull Jan 20 Articles

--- scripts/webscraping_articles.py
+++ scripts/webscraping_articles.py
@@ -18,24 +18,63 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from requests.exceptions import ConnectionError
 
 '''
 Script for scraping CBC news landing page for daily articles
 '''
-#Specifying news landing page and feeding into function to extract stories
 
-cbc_news = ['https://www.cbc.ca/news','https://www.cbc.ca/news/local']
+#Creating an empty pandas dataframe to store updated daily results
+df_store = pd.DataFrame(columns = ['author', 'date', 'title', 'maintext','mainurl','simlinks','relalinks'])
 
-cbc_
+#Creating a list of major CBC news landing pages
+cbc_landings = [
+        'https://www.cbc.ca/news',
+        'https://www.cbc.ca/news/canada/ottawa',
+        'https://www.cbc.ca/news/canada/british-columbia',
+        'https://www.cbc.ca/news/canada/calgary'
+        'https://www.cbc.ca/news/canada/edmonton',
+        'https://www.cbc.ca/news/canada/saskatchewan',
+        'https://www.cbc.ca/news/canada/montreal',
+        'https://www.cbc.ca/news/canada/new-brunswick',
+        'https://www.cbc.ca/news/canada/prince-edward-island',
+        'https://www.cbc.ca/news/canada/nova-scotia',
+        'https://www.cbc.ca/news/canada/newfoundland-labrador',
+        'https://www.cbc.ca/news/canada/north',
+        'https://www.cbc.ca/news/opinion',
+        'https://www.cbc.ca/news/world',
+        'https://www.cbc.ca/news/canada',
+        'https://www.cbc.ca/news/politics',
+        'https://www.cbc.ca/news/indigenous',
+        'https://www.cbc.ca/news/business',
+        'https://www.cbc.ca/news/health',
+        'https://www.cbc.ca/news/entertainment',
+        'https://www.cbc.ca/news/technology',
+        'https://www.cbc.ca/news/investigates',
+        'https://www.cbc.ca/news/gopublic'
+        ]
 
+#Looping Coverpage Scraping Function over cbc landing pages to get story urls
+cbc_home_lists = list(map(ScrapeCover, cbc_landings))
 
+#Flattening sublists of CBC news into a single list
+cbc_home_urls = []
+for sublist in cbc_home_lists:
+    for item in sublist:
+        cbc_home_urls.append(item)
 
-cbc_home = ScrapeCover(cbc_news)
+#Extacting unique set of links
+cbc_unique_stories = list(set(cbc_home_urls))
 
-cbc_data = ScrapeArticles(cbc_home)
+#Running article scraping function using list of isolated stories
+cbc_data = ScrapeArticles(cbc_unique_stories)
 
+#Append data for today to existing dataframe
+daily_export = df_store.append(cbc_data)
 
-export_csv = a.to_csv('test.csv')
+#Writing daily export file to csv
+daily_export.to_csv('Jan_20_news.csv')
+
 
 
 
