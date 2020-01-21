5b6cec43aa345944ba40a09d02218bef7002f253
cylitaguy <cylita.guy@gmail.com>
2020-01-20 19:56:33 -0500

    Created individual article scraping funtion, made updates to scraping script to run funcs

--- scripts/webscraping_articles.py
+++ scripts/webscraping_articles.py
@@ -12,73 +12,42 @@ Script contains functions that are meant to scrape the CBC news website for
 article data.
 
 '''
-import pandas as pd
+#Importing custom functions file
+import webscraping_funcs
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 '''
 Script for scraping CBC news landing page for daily articles
 '''
+#Specifying news landing page and feeding into function to extract stories
 
-#Specifying news landing page (here CBC.ca news) and converting to soup
-news_page = requests.get('https://www.cbc.ca/news')
-news_soup = BeautifulSoup(news_page.content, 'html5lib')
+cbc_news = ['https://www.cbc.ca/news','https://www.cbc.ca/news/local']
 
-#Extracting all news headlines from html soup
-coverpage_news = news_soup.find_all('a', class_='card')
-
-#Empyty list for storing story urls
-news_url = []
-
-#Extracting coverpage news story urls from html soup
-for story in np.arange(0, len(coverpage_news)):
-    url=coverpage_news[story]['href']
-    news_url.append(url)
-
-#Storing links in a dataframe and exporting as a csv
-url_DF = pd.DataFrame({'url':news_url})
-print(url_DF)
-#Tomorrow write this into a fucntion and then 
+cbc_
 
 
-#Now need to create a loop to extact info from news artcles of interest
-#Creating articles for information need to store:
 
-authorname = []
-date = []
-title = []
-url = []
-maintext = []
-related_links = []
+cbc_home = ScrapeCover(cbc_news)
+
+cbc_data = ScrapeArticles(cbc_home)
 
 
-for link in news_url:
-    #Requesting article via link and isolating html content
-    article = requests.get(link)
-    conent = article.content
-    
-    #Converting to soup
-
-test=news_url[4]
-print(test)    
-
-article = requests.get(test)
-content = article.content
-
-test_soup = BeautifulSoup(content, 'html5lib')    
-try:
-    author = test_soup.find('span', class_='authorText').get_text()
-except:
-    aname = "Anonymous"    
+export_csv = a.to_csv('test.csv')
 
 
-pub_time = test_soup.find('time', class_='timeStamp').get_text()   
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
     
     
     
--- scripts/webscraping_funcs.py
+++ scripts/webscraping_funcs.py
@@ -15,6 +15,7 @@ import pandas as pd
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 ###########Func 1
@@ -32,7 +33,7 @@ def ScrapeCover(url):
     coverpage_urls =[]
     
     #Extracting coverpage news urls from html soup
-    for story in np.arrange(0, len(headlines)):
+    for story in np.arange(0, len(headlines)):
         story_url = headlines[story]['href']
         coverpage_urls.append(story_url)
     
@@ -56,5 +57,90 @@ def ScrapeCover(url):
     return complete_urls
     
 #########Func 2
+#Function which requests specific details from article urls provide to it
+def ScrapeArticles(arts):
+    #Creating empty lists for article details that need to be stored
+    authname =[]
+    date = []
+    title = []
+    maintext = []
+    mainurl =[]
+    simlinks = []
+    relalinks = []
     
+    #Extracting relevant details from stories of interest
+    for link_no in np.arange(0, len(arts)):
+        #Requesting article link and creating content soup
+        link = arts[link_no]
+        #Adding in a pause in the code
+        time.sleep(3)
+        #Specify headers
+        headers = {
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+        art = requests.get(link, headers = headers)
+        art_soup = BeautifulSoup(art.content, 'html5lib')
     
+        #Add url to new list
+        mainurl.append(link)
+        
+        #Finding and adding authours
+        try:
+            author = art_soup.find('span', class_='authorText').get_text()
+        except:
+            author = "Anonymous"
+        
+        authname.append(author)
+        
+        #Finding and adding publication date
+        pub_time = art_soup.find('time', class_='timeStamp')['datetime']
+        date.append(pub_time)
+        
+        #Finding and adding article title
+        art_title = art_soup.find('h1', class_='detailHeadline').get_text()
+        title.append(art_title)
+        
+        #Finding and adding similar article links if present
+        try: 
+            s_links = art_soup.find_all('a', class_='similarLink')
+            s_urls_temp = []
+            s_urls = []
+            for similar in np.arange(0, len(s_links)):
+                linklist = s_links[similar]['href']
+                s_urls_temp.append(linklist)
+                s_urls = " ".join(s_urls_temp)    
+        except:
+            s_urls = "N/A"
+        
+        simlinks.append(s_urls)
+        
+        #Finding and adding related article links if present
+        try:
+            r_links=art_soup.find_all('a', class_='relatedLink')
+            r_urls_temp = []
+            for related in np.arange(0, len(r_links)):
+                linkrlist = r_links[related]['href']
+                r_urls_temp.append(linkrlist)
+                r_urls = " ".join(r_urls_temp)
+                
+        except:
+            r_urls = "N/A"
+        
+        relalinks.append(r_urls)
+        
+        #Scraping the actual paragraphs of the story of interest
+        body = art_soup.find_all('div', class_='story')
+        x = body[0].find_all('p')
+    
+        list_paras = []
+        for para in  np.arange(0, len(x)):
+            paragraph = x[para].get_text()
+            list_paras.append(paragraph)
+            final_article =" ".join(list_paras)
+            
+        maintext.append(final_article)
+            
+    #Now converting all lists into a pandas dataframe to be returned
+    df = pd.DataFrame(list(zip(authname, date,title, maintext, mainurl, simlinks, relalinks)), 
+                  columns = ['author', 'date', 'title', 'maintext','mainurl','simlinks','relalinks'])
+
+    return df
