80ec6798caf9162b8c7a86e06b1190cbd75e6752
cylitaguy <cylita.guy@gmail.com>
2020-01-22 18:18:19 -0500

    Wrote a function that computes cosine similarity for a query doc - compared to larger doc corpus

--- scripts/similiarity_funcs.py
+++ scripts/similiarity_funcs.py
@@ -11,6 +11,11 @@ Following script contains functions that will be used to compute similarity
 metricies for an inputed group of articles against the existing copora of text
 
 '''
+#Packages and functions
+from nltk.tokenize import word_tokenize
+import gensim
+import spacy
+import nltk
 
 #####Func 1
 #Function meant to isolate the first 30% of the cleaned story text (or sentances) for analysis
@@ -25,5 +30,42 @@ def Clean30 (text):
     #Return the joined text
     return First30_str
 
+#####Func 1
+#Function for creating the corpus of text that another story will be compared to
 
+def SimCorpCreate (corpora, directory, querydoc):
+    #Creating empty list to store the tokenized words for comparison from docs
+    token_comp = []
     
+    for doc in corpora:
+        tokens = word_tokenize(doc)
+        token_comp.append(tokens)
+    
+    #Creating a dictionary
+    dictionary = gensim.corpora.Dictionary(token_comp)
+    
+    #Creating corpus of words usign the dictionary
+    corpus = [dictionary.doc2bow(doc) for doc in token_comp]
+    
+    #Caclulating frequency–inverse document frequency
+    tf_idf = gensim.models.TfidfModel(corpus)
+    
+    #Building similiarity index to be saved wherever this script is
+    sims = gensim.similarities.Similarity(directory,tf_idf[corpus],
+                                        num_features=len(dictionary))
+    
+    #Tokenizing query text and creating a dictionary
+    querytokens = word_tokenize(querydoc)
+    query_doc_bow = dictionary.doc2bow(querytokens)
+    
+    #Creating a list to story query cosine similarity scores
+    cosine_simscore = []
+    
+    # perform a similarity query against the corpus
+    query_doc_tf_idf = tf_idf[query_doc_bow]
+    
+    #Attatch to list for outputs
+    cosine_simscore.append(sims[query_doc_tf_idf])
+    
+    return cosine_simscore
+
