0c55157e93b1237aa29641ded6c28905ef9693e8
cylitaguy <cylita.guy@gmail.com>
2020-01-30 18:32:30 -0500

    Updates to add another conditional statement that alters html ouptut based on article length

--- App2/server.py
+++ App2/server.py
@@ -14,6 +14,11 @@ sys.path.insert(1, '/Users/Cylita/Desktop/insight-ds-project_news-link/scripts')
 import text_normalization_funs as TN
 import similiarity_funcs as SIM
 
+#Load in corpora for comparison
+corpus = pd.read_csv('/Users/Cylita/Desktop/insight-ds-project_news-link/data/processed/Final_Cleaned_Corpus.csv')
+
+#Computing important story cutoffs
+
 
 # Create the application object
 app = Flask(__name__)
@@ -33,6 +38,63 @@ def recommendation_output():
 		return render_template("index.html",
                               	my_input = some_input,
                               	my_form_result="Empty")
+	
+	elif len(some_input) >= SIM.percentile_85:
+		
+		#Talking the comp text
+		comptext = some_input
+
+		#Cleaning the comptext to get it ready fo TF-IDF
+		cleancomp = TN.normalize_Story(story = comptext)
+
+		#Obtaining the first 30% of the comp text for longform story separation
+		#first30 = TN.Clean30(text = cleancomp)
+
+
+		############################################
+		####Performing 30% Story Similarity
+		#Computing similarity scores for given dataframe
+		#scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], 
+			#directory= '/Users/Cylita/Desktop/insight-ds-project_news-link/scripts/', 
+			#querydoc = first30)
+
+		#Top Stories
+		#Top_30_Stories = SIM.StoryMatch(SimScores = scores, originalDF = corpus)
+		
+		#Renaming the column headings of top stories
+		#Top_30_Stories.rename(columns={'title':'Article Title',
+                          #'mainurl':'Link',
+                          #'sim_scores':'Cosine Similarity'}, 
+                 #inplace=True)
+		
+		############################################
+		####Performing Whole Story Similarity
+		
+		#Computing similarity scores for given dataframe
+		scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], 
+			directory= '/Users/Cylita/Desktop/insight-ds-project_news-link/scripts/', 
+			querydoc = cleancomp)
+
+		#Top Stories
+		Top_Whole_Stories = SIM.StoryMatch(SimScores = scores, originalDF = corpus)
+
+		#Renaming the column headings of top stories
+		Top_Whole_Stories.rename(columns={'title':'Article Title',
+                          'mainurl':'Link',
+                          'sim_scores':'Cosine Similarity'}, 
+                 inplace=True)
+
+
+		return render_template("index.html",
+                              	tables=[#Top_30_Stories.to_html(index= False, classes='display',
+                              		#escape = False, render_links = True),
+                              	Top_Whole_Stories.to_html(index = False, 
+									classes='display', escape = False,
+									render_links = True)],
+                              	#Titles = ["For the Beginning of Your Article", "For the End"],
+                         		my_input = comptext,
+                              	my_form_result="Long")
+
 	else:
 		#Talking the comp text
 		comptext = some_input
@@ -40,8 +102,6 @@ def recommendation_output():
 		#Cleaning the comptext to get it ready fo TF-IDF
 		cleancomp = TN.normalize_Story(story = comptext)
 
-		#Load in corpora for comparison
-		corpus = pd.read_csv('/Users/Cylita/Desktop/insight-ds-project_news-link/data/processed/demo_corp.csv')
 
 		#Computing similarity scores for given dataframe
 		scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], 
@@ -57,18 +117,14 @@ def recommendation_output():
                           'sim_scores':'Cosine Similarity'}, 
                  inplace=True)
 		
-		#TopStories Titles
-		some_output = TopStories.to_html()
 		#Return my rendered template with the output
 		return render_template("index.html",
+							#my output
 							tables=[TopStories.to_html(index = False, 
 									classes='display', escape = False,
-									render_links = True)], 
-                          	#my_input=some_input,
-                          	my_output = some_output,
+									render_links = True)],
+                         	my_input = comptext,
                          	my_form_result="NotEmpty")
-
-
 
 # start the server with the 'run()' method
 if __name__ == "__main__":
