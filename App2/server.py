9cba0281733d6eb5769e4ef3d8a56f818ce1125b
cylitaguy <cylita.guy@gmail.com>
2020-01-30 15:51:19 -0500

    Updates to server functionality to display pandas dataframe as a table

--- App2/server.py
+++ App2/server.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import numpy as np
 import sys
+import json 
 
 #specifying local system path for remaining functions files
 sys.path.insert(1, '/Users/Cylita/Desktop/insight-ds-project_news-link/scripts')
@@ -27,7 +28,7 @@ def recommendation_output():
 	# Pull input
 	some_input = request.args.get('user_input')       	 
   	 
-	# Case if empty
+	# If the input is empty
 	if some_input =="":
 		return render_template("index.html",
                               	my_input = some_input,
@@ -43,24 +44,31 @@ def recommendation_output():
 		corpus = pd.read_csv('/Users/Cylita/Desktop/insight-ds-project_news-link/data/processed/demo_corp.csv')
 
 		#Computing similarity scores for given dataframe
-		scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], directory= '/Users/Cylita/Desktop/insight-ds-project_news-link/scripts/', querydoc = cleancomp)
+		scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], 
+			directory= '/Users/Cylita/Desktop/insight-ds-project_news-link/scripts/', 
+			querydoc = cleancomp)
 
 		#Top Stores
 		TopStories = SIM.StoryMatch(SimScores = scores, originalDF = corpus)
 
-		#Only Display the first two
-		TopStories2 = TopStories.iloc[0]
-
+		#Renaming the column headings of top stories
+		TopStories.rename(columns={'title':'Article Title',
+                          'mainurl':'Link',
+                          'sim_scores':'Cosine Similarity'}, 
+                 inplace=True)
+		
 		#TopStories Titles
-		some_output = TopStories2['title']
-		some_number = round(TopStories2['sim_scores'],2)
-		some_image="giphy.gif"
+		some_output = TopStories.to_html()
+		#Return my rendered template with the output
 		return render_template("index.html",
-                          	my_input=some_input,
+							tables=[TopStories.to_html(index = False, 
+									classes='display', escape = False,
+									render_links = True)], 
+                          	#my_input=some_input,
                           	my_output = some_output,
-                          	my_number=some_number,
-                          	my_img_name=some_image,
-                          	my_form_result="NotEmpty")
+                         	my_form_result="NotEmpty")
+
+
 
 # start the server with the 'run()' method
 if __name__ == "__main__":
