1f16d95d3cd6196e524aaa01291dc83208d6f913
cylitaguy <cylita.guy@gmail.com>
2020-02-08 23:39:15 -0500

    Cleaning up the code and annotation for the script that scrapes articles from the cbc.ca

--- scripts/webscraping_articles.py
+++ scripts/webscraping_articles.py
@@ -5,24 +5,14 @@ Created on Sun Jan 19 16:20:31 2020
 
 @author: Cylita
 """
+'''
+Script for scraping CBC news landing page for daily articles
 
 '''
-
-Script contains functions that are meant to scrape the CBC news website for
-article data.
-
-'''
-#Importing custom functions for analysis
+#Importing custom functions
 from webscraping_funcs import ScrapeCover
 from webscraping_funcs import ScrapeArticles
 import pandas as pd
-
-'''
-Script for scraping CBC news landing page for daily articles
-'''
-
-#Creating an empty pandas dataframe to store updated daily results
-df_store = pd.DataFrame(columns = ['author', 'date', 'title', 'maintext','mainurl','simlinks','relalinks'])
 
 #Creating a list of major CBC news landing pages
 cbc_landings = [
@@ -56,6 +46,7 @@ cbc_home_lists = list(map(ScrapeCover, cbc_landings))
 
 #Flattening sublists of CBC news into a single list
 cbc_home_urls = []
+
 for sublist in cbc_home_lists:
     for item in sublist:
         cbc_home_urls.append(item)
@@ -66,11 +57,8 @@ cbc_unique_stories = list(set(cbc_home_urls))
 #Running article scraping function using list of isolated stories
 cbc_data = ScrapeArticles(cbc_unique_stories)
 
-#Append data for today to existing dataframe
-daily_export = df_store.append(cbc_data)
-
 #Writing daily export file to csv
-daily_export.to_csv('Jan_21_news.csv')
+cbc_data.to_csv('Jan_27_news.csv')
 
 
 
