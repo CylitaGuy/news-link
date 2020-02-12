1ff7f28d31a96fd5c2008d1221ff51eba7cc376c
cylitaguy <cylita.guy@gmail.com>
2020-02-12 16:49:56 -0500

    Fixing errors with story length calculation and html rendering

--- news-link/server.py
+++ news-link/server.py
@@ -8,14 +8,13 @@ import sys
 import json 
 
 #specifying local system path for remaining functions files
-sys.path.insert(1, '/scripts')
 
 #self written functions and scripts that will be needed
-import text_normalization_funs as TN
-import similiarity_funcs as SIM
+import scripts.text_normalization_funs as TN
+import scripts.similiarity_funcs as SIM
 
 #Load in corpora for comparison
-corpus = pd.read_csv('/data/Final_Cleaned_Corpus.csv')
+corpus = pd.read_csv(r'data/Final_Cleaned_Corpus_test.txt')
 
 #Computing important story cutoffs
 
@@ -39,88 +38,37 @@ def recommendation_output():
                               	my_input = some_input,
                               	my_form_result="Empty")
 	
-	elif len(some_input) >= SIM.percentile_85:
-		
-		#Talking the comp text
+	else:
+		#Taking the comptext and cleaning it to get ready for TF-IDF
 		comptext = some_input
-
-		#Cleaning the comptext to get it ready fo TF-IDF
 		cleancomp = TN.normalize_Story(story = comptext)
 
-		#Obtaining the first 30% of the comp text for longform story separation
-		#first30 = TN.Clean30(text = cleancomp)
-
-
-		############################################
-		####Performing 30% Story Similarity
-		#Computing similarity scores for given dataframe
-		#scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], 
-			#directory= '/Users/Cylita/Desktop/insight-ds-project_news-link/scripts/', 
-			#querydoc = first30)
-
-		#Top Stories
-		#Top_30_Stories = SIM.StoryMatch(SimScores = scores, originalDF = corpus)
-		
-		#Renaming the column headings of top stories
-		#Top_30_Stories.rename(columns={'title':'Article Title',
-                          #'mainurl':'Link',
-                          #'sim_scores':'Cosine Similarity'}, 
-                 #inplace=True)
-		
-		############################################
-		####Performing Whole Story Similarity
-		
-		#Computing similarity scores for given dataframe
+		#Compute similarity scores
 		scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], 
-			directory= '/scripts/', 
-			querydoc = cleancomp)
-
-		#Top Stories
-		Top_Whole_Stories = SIM.StoryMatch(SimScores = scores, originalDF = corpus)
+									directory= '/Users/Cylita/Desktop/insight-ds-project_news-link/news-link/scripts/', 
+									querydoc = cleancomp)
+		#Give me Top Stories, rounded
+		Top_Whole_Stories = round(SIM.StoryMatch(SimScores = scores, originalDF = corpus),2)
 
 		#Renaming the column headings of top stories
 		Top_Whole_Stories.rename(columns={'title':'Article Title',
                           'mainurl':'Link',
                           'sim_scores':'Cosine Similarity'}, 
-                 inplace=True)
+                 			inplace=True)
 
-
-		return render_template("index.html",
-                              	tables=[#Top_30_Stories.to_html(index= False, classes='display',
-                              		#escape = False, render_links = True),
-                              	Top_Whole_Stories.to_html(index = False, 
+		if len(cleancomp) >= SIM.percentile_85:
+			return render_template("index.html",
+                              	tables=[Top_Whole_Stories.to_html(index = False, 
 									classes='display', escape = False,
 									render_links = True)],
-                              	#Titles = ["For the Beginning of Your Article", "For the End"],
                          		my_input = comptext,
                               	my_form_result="Long")
 
-	else:
-		#Talking the comp text
-		comptext = some_input
-
-		#Cleaning the comptext to get it ready fo TF-IDF
-		cleancomp = TN.normalize_Story(story = comptext)
-
-
-		#Computing similarity scores for given dataframe
-		scores = SIM.SimCorpCreate (corpora = corpus['cleaned_text'], 
-			directory= '/scripts/', 
-			querydoc = cleancomp)
-
-		#Top Stores
-		TopStories = SIM.StoryMatch(SimScores = scores, originalDF = corpus)
-
-		#Renaming the column headings of top stories
-		TopStories.rename(columns={'title':'Article Title',
-                          'mainurl':'Link',
-                          'sim_scores':'Cosine Similarity'}, 
-                 inplace=True)
-		
-		#Return my rendered template with the output
-		return render_template("index.html",
+		if len(cleancomp) < SIM.percentile_85:	
+			#Return my rendered template with the output
+			return render_template("index.html",
 							#my output
-							tables=[TopStories.to_html(index = False, 
+							tables=[Top_Whole_Stories.to_html(index = False, 
 									classes='display', escape = False,
 									render_links = True)],
                          	my_input = comptext,
